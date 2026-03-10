4261890b6d07acb104798981133af167b9976966
Ruben Perez <rubenperez038@gmail.com>
2026-03-10 18:29:36 +0100

    header

---
+++ 01-inline-functions/base64.hpp
@@ -0,0 +1,16 @@
+
+// File: base64.hpp
+
+#include <string>
+#include <cstddef>
+#include <span>
+
+namespace chat {
+
+inline std::string base64_encode(std::span<const std::byte> value)
+{
+    // ...
+    return {};
+}
+
+}
